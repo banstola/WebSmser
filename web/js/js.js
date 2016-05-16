7ef5ba644a979c9561af9200207b33551bdc0e73
Ram Krishna Banstola <ram.banstola@live.com>
2016-05-16 07:00:24 +0300

    Separate user interface from backend

--- web/js/js.js
+++ web/js/js.js
@@ -5,14 +5,42 @@ $(document).ready(function (){
      
      e.preventDefault();
      console.log("send sms");
-     number=$().
+     phoneNumber=$('#phone-number').val();
+     smstext= $('#sms-message').val();
+     posturl= "../src/DemoSend.php";
+
+    if(phoneNumber.length <= 0){
+        
+        
+        
+        
+    }
+  postAction=$.post( posturl, { "phone_number": phoneNumber, "sms_message": smstext });
+  
+  postAction.done(function (data){
+        $('#phone-number').val('');
+     $('#sms-message').val('');
+     console.log(data);
+   
+  });
+
+
  });
-
-
+var clicked_item=0;
  $('.view-message').on('click',function(e){
-     
+
+  if(clicked_item !==0){
+      $(clicked_item).removeClass('active');
+  }
      e.preventDefault();
      console.log("view individual message");
+     $(this).addClass('active');
+     message=$(this).data('message');
+     $('.show-message-details').css('display','block');
+     $('.show-message-details').html('<p>'+message+'</p>');
+    
+clicked_item=this;
+     
  });
 
 
